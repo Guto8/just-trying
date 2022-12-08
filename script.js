54f96d4814624098f53edd4317c38ca469f07179
Augusto Egea Lazzarin <augustoegealazzarin@gmail.com>
2022-12-08 15:55:34 -0300

    terminando?

---
+++ script.js
@@ -0,0 +1,18 @@
+var $lista = document.querySelector('ul');
+var $produto = document.querySelector('#produto');
+var $botao = document.querySelector('#btn');
+
+$botao.addEventListener('click', addproduto);
+
+function addproduto() {
+    var item = '<li>' + $produto.value + '</li>';
+    $lista.innerHTML +=item;
+    $produto.value = '';
+    $produto.focus();
+}
+
+$produto.addEventListener('keyup', function(e){
+   if (e.keyCode === 13) {
+      addproduto()
+   }
+})
